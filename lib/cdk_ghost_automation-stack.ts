34e6923f194563787e9b8fd2e45cb6a42cb613fe
Dassi Orleando <me@dassiorleando.xyz>
2022-02-13 19:59:19 -0500

    Created S3 bucket and RDS (MySQL) DB

--- lib/cdk_ghost_automation-stack.ts
+++ lib/cdk_ghost_automation-stack.ts
@@ -1,15 +1,63 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2'
+import { bucketName, vpcId, s3CorsRules as cors, ghostDbAdminPassword, ghostDbAdminUser, ghostDbName as databaseName } from './config';
 
+/**
+ * Ghost CDK Automation stack that creates an S3 bucket and a RDS (MySQL) database.
+ */
 export class CdkGhostAutomationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Creating the s3 bucket
+    new s3.Bucket(this, 'GhostS3Bucket', {
+      cors,
+      bucketName,
+      enforceSSL: true,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    
+    // Creating the RDS (MySQL) database in a VPC (default)
+    let vpcOptions: any = { isDefault: true }
+    if (vpcId) {
+      vpcOptions = { vpcId }
+    }
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', vpcOptions);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkGhostAutomationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dbInstance = new rds.DatabaseInstance(this, 'GhostRdsMySQL', {
+      databaseName,
+      publiclyAccessible: true,
+      multiAz: false,
+      instanceIdentifier: 'GhostRDSInstance',
+      credentials: rds.Credentials.fromPassword(ghostDbAdminUser, cdk.SecretValue.plainText(ghostDbAdminPassword)),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_5_7 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      }
+    });
+
+    // Some outputs including the credentials
+    new cdk.CfnOutput(this, 'ghostBucketName', {
+      value: bucketName,
+    });
+    new cdk.CfnOutput(this, 'ghostDbName', {
+      value: databaseName,
+    });
+    new cdk.CfnOutput(this, 'ghostDbEndpoint', {
+      value: dbInstance.instanceEndpoint.hostname,
+    });
+    new cdk.CfnOutput(this, 'ghostDbAdminUser', {
+      value: ghostDbAdminUser!,
+    });
+    new cdk.CfnOutput(this, 'ghostDbAdminPassword', {
+      value: ghostDbAdminPassword!,
+    });
   }
 }
---
+++ lib/config.ts
@@ -0,0 +1,36 @@
+/**
+ * Config file
+ */
+import { v4 as uuidv4 } from 'uuid';
+import { CorsRule, HttpMethods } from '@aws-cdk/aws-s3';
+
+// Some environment variables
+export const vpcId = process.env.CDK_VPC_ID;
+export const account: string = process.env.CDK_DEFAULT_ACCOUNT || 'aws-account';
+export const region: string = process.env.CDK_DEFAULT_REGION || 'aws-region';
+export const bucketName: string = process.env.CDK_DEFAULT_BUCKET_NAME || 's3-ghost-automation';
+
+// DB parameters
+export const ghostDbName: string = 'GhostDB';
+export const ghostDbAdminUser: string = 'ghost_admin';
+export const ghostDbAdminPassword: string = uuidv4();
+
+// S3 cors rule for bucket creation
+export const s3CorsRules: CorsRule[] = [
+    {
+        'allowedHeaders': [
+            '*'
+        ],
+        'allowedMethods': [
+          HttpMethods.HEAD,
+          HttpMethods.GET,
+          HttpMethods.PUT,
+          HttpMethods.POST,
+          HttpMethods.DELETE
+        ],
+        'allowedOrigins': [
+            '*'
+        ],
+        'exposedHeaders': []
+    }
+];
